b2ec8cb011a5536515704835b8a24d94f936e4c8
Nested <76160435+pundirvishal@users.noreply.github.com>
2025-03-25 18:10:38 +0530

    rep reset

--- src/pages/SignIn.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/clerk-react";
-
-export default function SignInPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <SignIn routing="path" path="/sign-in" />
-    </div>
-  );
-}
